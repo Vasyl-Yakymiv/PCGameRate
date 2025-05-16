d0fe9d32255d64c0808519ccedd531b2c018d208
Vasyl_Yakymiv <vassakimyv@gmail.com>
2025-05-16 15:49:55 +0400

    Add anti-spam rating restrictions

--- PCGameRate/PCGameRate/Controllers/RatingController.cs
+++ PCGameRate/PCGameRate/Controllers/RatingController.cs
@@ -22,13 +22,13 @@ namespace PCGameRate.Controllers
         
         public async Task<IActionResult> RateGame(int gameId)
         {
+          
             var game = await _context.Games.FindAsync(gameId);
             if (game == null)
             {
                 return NotFound();
             }
 
-            
             var userIdForCount = _userManager.GetUserId(User);
             var today = DateTime.UtcNow.Date;
             int ratingToday = await _context.Ratings
@@ -38,10 +38,28 @@ namespace PCGameRate.Controllers
                 TempData["ErrorMessage"] = "Ви вже оцінили 50 ігор на сьогодні. Повторіть спробу завтра.";
                 return RedirectToAction("Detail", "Game",new { id = gameId });
             }
+            var ratingDeviation = await _context.Ratings
+                .Where(u => u.Id == userIdForCount && u.RatingDate >= today)
+                .Take(30)
+                .Select(u => u.RatingValue.Value)
+                .ToListAsync();
+            if (ratingDeviation.Count >= 15)
+            {
+                var average = ratingDeviation.Average();
+                var variance = ratingDeviation.Average(r => Math.Pow(r - average, 2));
+                var stdDev = Math.Sqrt(variance); 
+
+                if (stdDev < 1.0) 
+                {
+                    TempData["ErrorMessage"] = " За вами виявлена підозріла активність.\n" +
+                        "Повторіть спробу оцінити гру завтра.";
+                    return RedirectToAction("Detail", "Game", new { id = gameId });
+                }
+            }
             var userId = User.Identity.Name;
             var existingRating = await _context.Ratings
                 .FirstOrDefaultAsync(r => r.GameId == gameId && r.User.UserName == userId);
-
+            
             ViewBag.Game = game;
             ViewBag.ExistingRating = existingRating?.RatingValue;
 
@@ -52,7 +70,7 @@ namespace PCGameRate.Controllers
         [HttpPost]
         public async Task<IActionResult> RateGame(int gameId, int ratingValue)
         {
-           
+            var stopwatch = Stopwatch.StartNew();
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             if (string.IsNullOrEmpty(userId))
             {
@@ -88,6 +106,9 @@ namespace PCGameRate.Controllers
                 game.RatingAverage = ((game.RatingAverage * (game.RatingCount - 1)) + ratingValue) / game.RatingCount;
             }
             await _context.SaveChangesAsync();
+            stopwatch.Stop();
+            var elapsedMs = stopwatch.ElapsedMilliseconds;
+            TempData["ErrorMessage"] = $"{elapsedMs} мс";
             return RedirectToAction("Detail", "Game", new { id = gameId });
         }
         [HttpGet]
